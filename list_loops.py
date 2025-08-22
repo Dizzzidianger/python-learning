ee7d7c1cc24f18c79d7c6faab2dd2ac8ed222f5a
Nalo262 <dizzidanger2@mail.ru>
2025-08-22 16:59:04 +0300

    List_loops

---
+++ list_loops.py
@@ -0,0 +1,4 @@
+games = ("Valheim","Guild Wars 2 ","City of Heroes: Homecoming","Dave the Diver,""Dragon Age: Origins","Left 4 Dead 2"  )
+for index, game in enumerate(games, start=1):
+    print (f"{index}, {game}")
+        
